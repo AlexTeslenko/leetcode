da99c6dcc84b8b0abff4a6d3b41d25bc3d1c0c73
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-13 11:32:40 +0200

    Time: 683 ms (98.60%), Space: 31.6 MB (42.39%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,14 +1,15 @@
-from collections import deque
 
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        buildings = deque([])
+        buildings = []
         cur_max_building = -1
         
         for i in range(len(heights) - 1, -1, -1):
             if heights[i] > cur_max_building:
                 cur_max_building = heights[i]
-                buildings.appendleft(i)
+                buildings.append(i)
+        
+        buildings.reverse()
         
         return buildings
             
