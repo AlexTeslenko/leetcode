1c5ada6c1dbe4b383c97c968a607c25b606dd8b9
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-19 20:31:00 +0200

    Time: 63 ms (13.66%), Space: 13.9 MB (10.59%) - LeetHub

---
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,17 @@
+from collections import deque
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = deque()
+        num_right_parent = 0
+        for par in s:
+            if par == '(':
+                stack.append(par)
+            else:
+                if len(stack):
+                    stack.pop()
+                else:
+                    num_right_parent += 1
+        
+        print(num_right_parent)
+        return len(stack) + num_right_parent
+        
