dd2fca2182640caf0a2fd70cea08e28c2bd5bf37
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-28 10:51:26 +0200

    Time: 292 ms (97.17%), Space: 20.3 MB (56.36%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
@@ -5,12 +5,12 @@ class Solution:
         while low <= high:
             middle = low + (high - low) // 2
             curr_missed = nums[middle] - zero_idx_el - middle
-            print(middle)
+    
             if curr_missed >= k:
                 high = middle - 1
             else:
                 low = middle + 1
 
-        return nums[low-1] + k - (nums[low-1] - zero_idx_el - (low-1))
+        return nums[high] + k - (nums[high] - zero_idx_el - (high))
 
         
