c37a11a425371e2cf86b1981c90b66062c054d14
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-28 10:47:16 +0200

    Time: 608 ms (9.76%), Space: 20.5 MB (26.83%) - LeetHub

---
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        zero_idx_el = nums[0]
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            middle = low + (high - low) // 2
+            curr_missed = nums[middle] - zero_idx_el - middle
+            print(middle)
+            if curr_missed >= k:
+                high = middle - 1
+            else:
+                low = middle + 1
+        
+        print('------')
+        print(low)
+        print(middle)
+        print((nums[middle-1] - zero_idx_el - (middle-1)))
+        if middle > 0:
+            return nums[low-1] + k - (nums[low-1] - zero_idx_el - (low-1))
+        else:
+            return nums[low-1] + k
+        
