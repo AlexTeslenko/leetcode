4bc948bc4c2cc2c7893f741f657602bb435e5560
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-28 13:21:52 +0200

    Time: 134 ms (30.50%), Space: 14.2 MB (34.40%) - LeetHub

---
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
@@ -0,0 +1,27 @@
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        ids_total_time = {i:0 for i in range(n)}
+        stack = []
+        i = 0
+        while i < len(logs):
+            cur_log = logs[i].split(':')
+            cur_idx = int(cur_log[0])
+            op = cur_log[1]
+            cur_time = int(cur_log[2])
+            print(op)
+            if op == 'start':
+                stack.append([cur_idx, op, cur_time])
+            else:
+                prev_log = stack.pop()
+                total_time = cur_time - prev_log[2] + 1
+                ids_total_time[cur_idx] += total_time
+                
+                if stack:
+                    ids_total_time[stack[-1][0]] -= total_time
+            
+            i += 1
+            
+        return list(ids_total_time.values())
+                
+
+        
