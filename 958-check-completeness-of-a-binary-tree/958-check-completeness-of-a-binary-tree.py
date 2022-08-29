af374456a3ba987ff0b6ee360c8b0add8eba1285
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-29 12:20:33 +0200

    Time: 37 ms (93.15%), Space: 13.8 MB (71.18%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
@@ -4,9 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
+from collections import deque
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        return self.method_2(root)
+        '''
         i = 0
         queue = [root]
         while queue[i]:
@@ -18,4 +20,23 @@ class Solution:
             i += 1
         
         return not any(queue[i:])
+        '''
+    
+    def method_2(self, root):
+        queue = deque([root])
+        prev_node = root
+        while queue:
+            cur_node = queue.popleft()
+            
+            if cur_node:
+                queue.append(cur_node.left)
+                queue.append(cur_node.right)
+            
+                if prev_node is None:
+                    return False
+            
+            prev_node = cur_node
+        
+        return True
+            
                 
