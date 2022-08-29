1264529c29288aaef68cebc3bac2cbb9ef3decf5
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-29 12:39:56 +0200

    Time: 260 ms (33.73%), Space: 14.6 MB (17.49%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if len(s) <= 1:
+            return True
+        
+        left_ptr, right_ptr = 0, len(s)-1
+        can_remove_ch = True
+        
+        while left_ptr < right_ptr:
+            if s[left_ptr] == s[right_ptr]:
+                left_ptr += 1
+                right_ptr -= 1
+            elif  s[left_ptr] != s[right_ptr]:
+                return self.check_palindrom(s, left_ptr+1, right_ptr) or self.check_palindrom(s, left_ptr, right_ptr-1)
+        
+        return True
+    
+    def check_palindrom(self, s, left_ptr, right_ptr):
+        while left_ptr < right_ptr:
+            if s[left_ptr] == s[right_ptr]:
+                left_ptr += 1
+                right_ptr -= 1
+            else:
+                return False
+        return True
+            
+            
