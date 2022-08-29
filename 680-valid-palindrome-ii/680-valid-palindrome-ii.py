19932dfb0adbaa9055b6195e97c3172ab7a170aa
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-29 12:41:50 +0200

    Time: 248 ms (37.46%), Space: 14.5 MB (47.64%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -22,5 +22,27 @@ class Solution:
             else:
                 return False
         return True
+    
+    '''
+        s_revert = s[::-1]
+        i, j = 0, len(s) - 1
+    
+        while i <= j:
+            if s[i] != s[j]:
+                return self.check_palindrom(s, i+1, j) or self.check_palindrom(s, i, j-1)
+            i += 1
+            j -= 1
+        return True
+
+    
+    def check_palindrom(self, s, i, j):
+        while i <= j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
+    '''
             
             
