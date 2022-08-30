714618a520917f974d63c73038d64d7510e8c5e3
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-08-30 21:19:14 +0200

    Time: 712 ms (13.05%), Space: 22.6 MB (14.28%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,5 +1,29 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        total_polinfromes = 0 
+        dp = [[False for _ in s] for i in s]
+        
+        for i in range(len(s)):
+            total_polinfromes += 1
+            dp[i][i] = True
+        
+        for i in range(0, len(s)-1):
+            dp[i][i+1] = s[i] == s[i+1]
+            total_polinfromes += dp[i][i+1]
+        
+        for ln in range(3, len(s)+1):
+            i = 0
+            j = i + ln - 1
+            while j < len(s):
+                dp[i][j] = dp[i+1][j-1] and s[i] == s[j]
+                total_polinfromes += dp[i][j]
+                i += 1
+                j += 1
+   
+        return total_polinfromes
+    
+        
+        '''
         num_paindrs = 0
         for i in range(len(s)):
             for j in range(i+1, len(s)+1):
@@ -7,3 +31,7 @@ class Solution:
                     #print(s[i:j])
                     num_paindrs += 1
         return num_paindrs
+        '''
+    
+    
+ 
