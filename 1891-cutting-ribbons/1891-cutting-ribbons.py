1fe2f967e86c2ca11ba927f9039768d028705087
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-04 09:55:31 +0200

    Time: 1964 ms (70.02%), Space: 28.5 MB (17.97%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        
+        def check_feasible(ribbons, length):
+            num_ribbons = 0
+            for i in range(len(ribbons)):
+                cur_ribbon = ribbons[i]
+                num_ribbons += cur_ribbon // length
+            
+            print(length, num_ribbons)
+            return num_ribbons
+        
+        if sum(ribbons) < k :
+            return 0
+        
+        low, high = 1, max(ribbons)
+        max_lenght = 0 
+        while low <= high:
+            middle = low + (high - low) // 2
+            cur_num_ribbons = check_feasible(ribbons, middle)
+            #print(cur_num_ribbons)
+            if cur_num_ribbons < k:
+                high = middle - 1 
+            else:
+                max_lenght = middle
+                low = middle + 1
+        
+        return max_lenght
+            
+        
+                    
+        
