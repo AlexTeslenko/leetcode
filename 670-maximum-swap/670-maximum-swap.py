7cdbf512934e96316082fcbdd646802d9dcca277
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-05 10:58:08 +0200

    Time: 44 ms (58.15%), Space: 13.8 MB (70.87%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -1,14 +1,9 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         digits = [int(ch) for ch in str(num)]
-        #cur_num = num
-        #while cur_num:
-        #    last_digit = cur_num % 10
-        #    digits.append(last_digit)
-        #    cur_num //= 10
-            
+
         idx_to_swap, digit_to_swap = -1, -1
-        #digits.reverse()
+
         print(digits)
         for i in range(1, len(digits)):
             if digits[i] > digits[i-1]:
@@ -30,20 +25,11 @@ class Solution:
                     idx_to_swap = i
             digits[idx_to_swap], digits[max_idx] = digits[max_idx],  digits[idx_to_swap]
                     
-        
-        
-        #if idx_to_swap != -1:
-        #    max_num_to_swap = max(digits[idx_to_swap+1:])
-        #    max_idx = digits[idx_to_swap+1:].index(max_num_to_swap) + idx_to_swap + 1
-        #    print(max_idx)
-        #    digits[idx_to_swap], digits[max_idx] = digits[max_idx],  digits[idx_to_swap]
-        
+
         print(digits)
         new_num = 0
         for digit in digits:
             new_num = new_num * 10 + digit
-        
-          
-        
+                
         return new_num
         
