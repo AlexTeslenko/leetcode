16998e3dc160bf8936098afac7b32a8f4c330aab
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-05 14:01:25 +0200

    Time: 213 ms (15.96%), Space: 15.7 MB (36.70%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -5,51 +5,55 @@ class Solution:
         stack = deque()
         i = 0
         while i < len(s):
-            ch = s[i]
-            if ch == " ":
-                i += 1
-            elif ch.isnumeric():
-                cur_ch = ""
-                while  i < len(s) and s[i].isnumeric():
-                    cur_ch = cur_ch + s[i]
-                    i += 1 
-                stack.append(cur_ch)
-            elif ch in ['+', '-']:
-                stack.append(ch)
-                i += 1
-            else:
-                num_1 = int(stack.pop())
-                while not s[i].isnumeric():
+            #print(stack)
+            #print('idx: ', i)
+            #print('ch: ', s[i])
+            if s[i].isnumeric():
+                ch = ''
+                while i < len(s) and s[i].isnumeric():
+                    ch = ch + s[i]
                     i += 1
-                cur_ch = ""
-                while  i < len(s) and s[i].isnumeric():
-                    cur_ch = cur_ch + s[i]
-                    i += 1 
-                num_2 = int(cur_ch)
-
-                cur_res = 0
-                if ch == '/':
-                    cur_res = num_1 // num_2
-                elif ch == '*':
-                    cur_res = num_1 * num_2
-                
-                stack.append(str(cur_res))
-
-        cur_res = int(stack.popleft())
-        cur_op = '+'
-        while stack:
-            ch = stack.popleft()
-            if ch.isnumeric():
-                if cur_op == '+':
-                    cur_res += int(ch)
+                stack.append(int(ch))
+            elif s[i] == '-':
+                i += 1
+                while i < len(s) and not s[i].isnumeric():
+                    i += 1
+                ch = ''
+                while i < len(s) and s[i].isnumeric():
+                    ch = ch + s[i]
+                    i += 1
+                stack.append(-int(ch))
+            elif s[i] == '+':
+                i += 1
+            elif s[i] in ['*', '/']:
+                op = s[i]
+                i += 1
+                #print(i)
+                while i < len(s) and not s[i].isnumeric():
+                    i += 1
+                #print(i)
+                ch = ''
+                while i < len(s) and s[i].isnumeric():
+                    ch = ch + s[i]
+                    i += 1
+                num_2 = int(ch)
+                #print(num_2)
+                num_1 = stack.pop()
+                if op == '*':
+                    stack.append(num_1 * num_2)
                 else:
-                    cur_res -= int(ch)
-            elif ch == '+':
-                cur_op = '+'
-            else:
-                cur_op = '-'
+                    #print(num_1 // num_2)
+                    stack.append(int(num_1 / num_2))
+                #i += 1
+            elif s[i] == ' ':
+                i += 1
+            #else:
+            #    i += 1
         
-        return cur_res
+        #print(stack)
+        return sum(stack)
+                
+                
                     
                 
                 
