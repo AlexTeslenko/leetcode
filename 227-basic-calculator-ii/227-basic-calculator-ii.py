747e83391c294401438f9ea42f669801eb89e10a
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-05 14:02:28 +0200

    Time: 145 ms (46.68%), Space: 15.7 MB (52.50%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -5,9 +5,6 @@ class Solution:
         stack = deque()
         i = 0
         while i < len(s):
-            #print(stack)
-            #print('idx: ', i)
-            #print('ch: ', s[i])
             if s[i].isnumeric():
                 ch = ''
                 while i < len(s) and s[i].isnumeric():
@@ -28,29 +25,21 @@ class Solution:
             elif s[i] in ['*', '/']:
                 op = s[i]
                 i += 1
-                #print(i)
                 while i < len(s) and not s[i].isnumeric():
                     i += 1
-                #print(i)
                 ch = ''
                 while i < len(s) and s[i].isnumeric():
                     ch = ch + s[i]
                     i += 1
                 num_2 = int(ch)
-                #print(num_2)
                 num_1 = stack.pop()
                 if op == '*':
                     stack.append(num_1 * num_2)
                 else:
-                    #print(num_1 // num_2)
                     stack.append(int(num_1 / num_2))
-                #i += 1
             elif s[i] == ' ':
                 i += 1
-            #else:
-            #    i += 1
-        
-        #print(stack)
+
         return sum(stack)
                 
                 
