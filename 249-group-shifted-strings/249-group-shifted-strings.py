053273ea83b00c4ccd288f168c81a73f7a8119c8
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-06 11:03:17 +0200

    Time: 43 ms (83.83%), Space: 14.1 MB (10.14%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -1,17 +1,19 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         hash_maps = {}
-        for string in strings:
-            shift = ord(string[0])
+        for s in strings:
+            first_ch = s[0]
+            first_num = (ord(first_ch) - ord('a'))
+            num = ''
+            for ch in s:
+                cur_num = (ord(ch) - first_num) % 26
+                print(cur_num)
+                num = num + str(cur_num)
             
-            shifted_str = ''
-            for ch in string:
-                shifted_str += str((ord(ch) - shift) % 26 - ord('a')) 
+            if num not in hash_maps:
+                hash_maps[num] = []
+            hash_maps[num].append(s)
+        
+        print(hash_maps)
+        return hash_maps.values()
             
-            if shifted_str not in hash_maps:
-                hash_maps[shifted_str] = []
-            hash_maps[shifted_str].append(string)
-        
-        result = [value for value in hash_maps.values()]
-        
-        return result
