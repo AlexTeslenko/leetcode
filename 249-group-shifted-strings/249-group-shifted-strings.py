33a9424432d4ab6039d3831da31eabb0f356e2f5
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-06 11:04:58 +0200

    Time: 74 ms (18.14%), Space: 13.9 MB (44.63%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -15,3 +15,20 @@ class Solution:
         
         return hash_maps.values()
             
+        '''
+        hash_maps = {}
+        for string in strings:
+            shift = ord(string[0])
+            
+            shifted_str = ''
+            for ch in string:
+                shifted_str += str((ord(ch) - shift) % 26 - ord('a')) 
+            
+            if shifted_str not in hash_maps:
+                hash_maps[shifted_str] = []
+            hash_maps[shifted_str].append(string)
+        
+        result = [value for value in hash_maps.values()]
+        
+        return result
+        '''
