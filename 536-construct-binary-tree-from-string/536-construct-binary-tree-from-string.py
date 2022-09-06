e4df9ebd393659179f158811561a65eca1596a18
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-06 17:28:32 +0200

    Time: 237 ms (35.73%), Space: 15.1 MB (29.03%) - LeetHub

---
+++ 536-construct-binary-tree-from-string/536-construct-binary-tree-from-string.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def str2tree(self, s: str) -> Optional[TreeNode]:
+        def get_num(s, indx):
+            is_negative = False
+            if s[indx] == '-':
+                is_negative = True
+                indx += 1
+            
+            val = 0
+            while indx < len(s) and s[indx].isnumeric():
+                val = val * 10 + int(s[indx])
+                indx += 1
+            
+            if is_negative:
+                val = -val
+                
+            return val, indx
+        
+        def str_to_tree(s, indx):
+            if indx == len(s):
+                return None, indx
+            
+            value, indx = get_num(s, indx)
+            
+            root = TreeNode(value)
+            
+            if indx < len(s) and s[indx] == '(':
+                root.left, indx = str_to_tree(s, indx+1)
+                
+            if root.left and indx < len(s) and s[indx] == '(':
+                root.right, indx = str_to_tree(s, indx+1)
+                
+            #if indx < len(s) and s[indx] == ')':
+            #    return root, indx+1
+            #else:
+            #    return indx
+            
+            return root, indx+1
+            
+        root, indx =  str_to_tree(s, 0)
+        return root
