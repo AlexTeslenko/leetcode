74a915c28f173353e7077ac266261670c51b57d5
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-07 13:52:07 +0200

    Time: 84 ms (5.51%), Space: 15.3 MB (32.10%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -19,23 +19,6 @@ class Solution:
         self.tile.right = self.head
         self.head.left = self.tile
         
-        
-        node = self.head
-       # while node != self.tile:
-       #     print("node: ", node.val)
-       #     print("left: ", node.left.val)
-       #     print("right: ", node.right.val)
-       #     node = node.right
-        
-        #print("node: ", self.tile.val)
-        #print("left: ", self.tile.left.val)
-        #print("right: ", self.tile.right.val)
-        #print(self.head.val)
-        #print(self.tile.val)
-        
-        #self.new_head = Node(0, None, self.head)
-        
-        
         return self.head
         
     
@@ -47,10 +30,12 @@ class Solution:
             if not self.head:
                 self.head = root
                 
-        print(root.val)
+        
         self.dfs2(root.left)
         
         if self.tile:
+            print("self.tile.val: ", self.tile.val)
+            print("root: ", root.val)
             self.tile.right = root
             root.left =  self.tile
         
@@ -58,49 +43,5 @@ class Solution:
         
         self.dfs2(root.right)
         
-        #self.tile.left = root
-        
-        
-        
-         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    def dfs(self, root):
-        if root is None:
-            return
-        
-        if not root.left:
-            if not self.head:
-                self.head = root
-        
-                
-        left_node = self.dfs(root.left)
-        
-        
-        if left_node:
-            left_node.right = root
-            root.left = left_node
-        
-        right_node = self.dfs(root.right)
-        
-        if right_node:
-            root.right = right_node
-            right_node.left = root
-        
-        if not root.right:
-            self.tile = root
-        
-        return right_node if right_node and left_node else root
-            
         
         
