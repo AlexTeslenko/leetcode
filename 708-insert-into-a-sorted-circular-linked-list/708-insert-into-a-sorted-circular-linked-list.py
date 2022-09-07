c9aa4c1477bf00c849a6c4610b5d899652b6a9bf
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-07 14:31:02 +0200

    Time: 53 ms (65.45%), Space: 14.9 MB (12.56%) - LeetHub

--- 708-insert-into-a-sorted-circular-linked-list/708-insert-into-a-sorted-circular-linked-list.py
+++ 708-insert-into-a-sorted-circular-linked-list/708-insert-into-a-sorted-circular-linked-list.py
@@ -8,10 +8,42 @@ class Node:
 
 class Solution:
     def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        new_node = Node(insertVal)
+        new_node.next = new_node
+        if not head:
+            return new_node
+        
+        prev_node = head
+        cur_node = head.next
+        to_insert = False
+        while True:
+            if (prev_node.val <= new_node.val <= cur_node.val):     
+                to_insert = True
+            elif prev_node.val > cur_node.val:
+                if new_node.val >= prev_node.val or new_node.val <= cur_node.val:
+                    to_insert = True
+            
+            if to_insert:
+                print(cur_node.val)
+                new_node.next = prev_node.next
+                prev_node.next = new_node
+                return head
+            
+            prev_node = cur_node
+            cur_node = cur_node.next
+            
+            if prev_node == head:
+                break
+        
+        new_node.next = cur_node
+        prev_node.next = new_node
+        
+        return head
+
         if not head:
             new_node = Node(insertVal)
             new_node.next = new_node
-            return  new_node
+            return new_node
         
         prev, curr = head, head.next
         to_insert = False
