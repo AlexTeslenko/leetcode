5c707afb07baafccf48a16c2a146ae81bd72101b
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-08 13:31:57 +0200

    Time: 184 ms (8.48%), Space: 26.6 MB (17.06%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,34 +7,38 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        lca = TreeNode(None)
-        self.dfs(root, p, q, lca)
+        self.p = p.val
+        self.q = q.val
+        self.lca = None
+        found_p_q = [False, False]
+        self.dfs(root)
         
-        return lca
-    
-    def dfs(self, root, p, q, lca):
-        if root is None:
-            return False
+        return self.lca
         
-        left_nod, right_nod = [False, False], [False, False]
         
-        if root.left:
-            left_nod = self.dfs(root.left, p, q, lca)
         
-        if root.right:
-            right_nod = self.dfs(root.right, p, q, lca)
+    def dfs(self, root):
+        if not root:
+            return [False, False]
+        
+        found_p_q_1 = self.dfs(root.left)
+        found_p_q_2 = self.dfs(root.right)
+        
+        found_p_q = [False, False]
+        if root.val == self.p or found_p_q_1[0] or found_p_q_2[0]:
+            found_p_q[0] = True
+        
+        if root.val == self.q or found_p_q_1[1] or found_p_q_2[1]:
+            found_p_q[1] = True
             
-        found_p, found_q = False, False
+        if found_p_q[0] and found_p_q[1] and not self.lca:
+            self.lca = root
         
-        if root.val == p.val or left_nod[0] or right_nod[0]:
-            found_p = True
+        print(found_p_q)
+        print(root.val)
         
-        if root.val == q.val or left_nod[1] or right_nod[1]:
-            found_q = True
+        return found_p_q
         
-        #print(found_p, found_q, root.val, p.val, q.val)
-        if found_p and found_q and lca.val == None:
-            lca.val = root.val
             
-        return [found_p, found_q]
-            
+        
+        
