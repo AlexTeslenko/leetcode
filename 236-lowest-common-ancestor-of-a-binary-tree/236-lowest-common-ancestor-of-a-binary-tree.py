c4b0073935d352e77c343e7bae5384b5d0c137ee
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-08 13:33:34 +0200

    Time: 182 ms (8.68%), Space: 26.8 MB (15.50%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -35,7 +35,41 @@ class Solution:
         
 
         return found_p_q
+    
+    
+    '''
+    
+        lca = TreeNode(None)
+        self.dfs(root, p, q, lca)
         
+        return lca
+    
+    def dfs(self, root, p, q, lca):
+        if root is None:
+            return False
+        
+        left_nod, right_nod = [False, False], [False, False]
+        
+        if root.left:
+            left_nod = self.dfs(root.left, p, q, lca)
+        
+        if root.right:
+            right_nod = self.dfs(root.right, p, q, lca)
+            
+        found_p, found_q = False, False
+        
+        if root.val == p.val or left_nod[0] or right_nod[0]:
+            found_p = True
+        
+        if root.val == q.val or left_nod[1] or right_nod[1]:
+            found_q = True
+        
+        #print(found_p, found_q, root.val, p.val, q.val)
+        if found_p and found_q and lca.val == None:
+            lca.val = root.val
+            
+        return [found_p, found_q]
+    '''
             
         
         
