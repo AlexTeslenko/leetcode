c88e570c2625a712cb93e87e067ad11ac6ccf45b
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-09 13:01:04 +0200

    Time: 47 ms (53.69%), Space: 14 MB (18.05%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.py
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -1,20 +1,22 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        ch_order = {ch:i for i, ch in enumerate(order)}
-        s_ordered = [[] for ch in order]
-        
-        new_s = ""
+        s_map = {ch: 0 for ch in s}
         for ch in s:
-            if ch in ch_order:
-                s_ordered[ch_order[ch]].append(ch)
-            else:
-                new_s = new_s + ch
+            s_map[ch] += 1
+            
+        ordered_s = ''
         
-        ordered_s = ""
-        for el in s_ordered:
-            if len(el):
-                for ch in el:
-                    ordered_s = ordered_s + ch
+        for ch in order:
+            if ch in s_map:
+                new_s = ch * s_map[ch]
+                del s_map[ch]
+                ordered_s += new_s
         
-        return ordered_s + new_s
+        for ch in s_map:
+            new_s = ch * s_map[ch]
+            ordered_s += new_s
+        
+        return ordered_s
+        
+        
         
