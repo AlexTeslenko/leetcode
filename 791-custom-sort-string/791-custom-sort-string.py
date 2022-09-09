344c9aab504b2ff188ed1f048225a1eb24241188
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-09 13:01:56 +0200

    Time: 56 ms (28.68%), Space: 13.9 MB (67.42%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.py
+++ 791-custom-sort-string/791-custom-sort-string.py
@@ -17,6 +17,25 @@ class Solution:
             ordered_s += new_s
         
         return ordered_s
+    
+    '''
+        ch_order = {ch:i for i, ch in enumerate(order)}
+        s_ordered = [[] for ch in order]
         
+        new_s = ""
+        for ch in s:
+            if ch in ch_order:
+                s_ordered[ch_order[ch]].append(ch)
+            else:
+                new_s = new_s + ch
+        
+        ordered_s = ""
+        for el in s_ordered:
+            if len(el):
+                for ch in el:
+                    ordered_s = ordered_s + ch
+        
+        return ordered_s + new_s
+    '''
         
         
