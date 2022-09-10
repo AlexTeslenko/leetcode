4a39dc380bf86520b5cea8b626b916b0d80bd414
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-10 20:40:18 +0200

    Time: 232 ms (10.26%), Space: 14.2 MB (88.30%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
@@ -14,6 +14,28 @@ class Solution:
         cur_row_is_zero = False
         for row in range(rows):
             if binaryMatrix.get(row, col_start_postion) != 0:
+                low, high = 0, col_start_postion
+                while low < high:
+                    middle = low + (high - low) // 2
+                    if binaryMatrix.get(row, middle) == 1:
+                        high = middle
+                    else:
+                        low = middle + 1
+                if binaryMatrix.get(row, high) == 1:
+                    first_one_col = min(first_one_col, high)
+                    col_start_postion = high
+                             
+        
+        return first_one_col if first_one_col < 10000 else -1
+    
+    
+    '''
+        rows, cols = binaryMatrix.dimensions()
+        first_one_col = 10000
+        col_start_postion = cols - 1
+        cur_row_is_zero = False
+        for row in range(rows):
+            if binaryMatrix.get(row, col_start_postion) != 0:
                 col = col_start_postion
                 cur_row_is_zero = False
                 while col >= 0 and not cur_row_is_zero:
@@ -28,3 +50,4 @@ class Solution:
                 col_start_postion = col+1               
         
         return first_one_col if first_one_col < 10000 else -1
+    '''
