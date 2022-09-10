7cbf275685640682480ea6d1328661c7b7225fcc
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-10 10:49:58 +0200

    Time: 1870 ms (5.10%), Space: 14.3 MB (64.66%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,33 +1,28 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] != 0:
+        dirs = [(1, 0), (1,-1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+        min_path_lenght = 1000
+        if grid[0][0] == 1:
             return -1
         
-        visited = set()
-        queue = deque()
-        queue.append((0, 0, 1))
-        diretions = [(-1,-1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
-        min_path = math.inf
-        
+        queue = deque([(0,0,1)])
+        grid[0][0] = 1
         while queue:
-            cur_node = queue.popleft()
-            if cur_node[0] == len(grid) - 1 and cur_node[1] == len(grid[0]) - 1:
-                min_path = min(min_path, cur_node[2])
-                
-            if (cur_node[0], cur_node[1]) not in visited:
-                for direction in diretions:
-                    new_node = (cur_node[0] + direction[0], cur_node[1] + direction[1])
-                    if new_node[0] >= 0 and new_node[0] < len(grid) and new_node[1] >=0 and new_node[1] < len(grid[0]):
-                        if grid[new_node[0]][new_node[1]] == 0:
-                            queue.append((new_node[0], new_node[1], cur_node[2]+1))
+            row, col, path_ln = queue.popleft()
             
-            visited.add((cur_node[0], cur_node[1]))
+            if row == len(grid)-1 and col == len(grid[0])-1:
+                min_path_lenght = min(min_path_lenght, path_ln)
+            
+            for dr in dirs:
+                new_row = row + dr[0]
+                new_col = col + dr[1]
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]) and grid[new_row][new_col] == 0:
+                    queue.append((new_row, new_col, path_ln+1))
+                    grid[new_row][new_col] = 1
         
-        if min_path == math.inf:
-            return -1
-        
-        return min_path
+        return min_path_lenght if min_path_lenght != 1000 else -1
+            
             
         
         
