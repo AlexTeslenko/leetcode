bf4b3b23460bb2b5024ab190a855b1c53939b0d6
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-12 13:36:09 +0200

    Time: 190 ms (97.68%), Space: 17.5 MB (68.14%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.py
+++ 498-diagonal-traverse/498-diagonal-traverse.py
@@ -33,64 +33,44 @@ class Solution:
             output += cur_diag
                 
         return output
-            
-            
-                
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+         
+
     ''' 
-        
         output = []
-        rows, cols = len(mat), len(mat[0])
-        i, j = 0, 0
-        down = False
-        while len(output) < rows * cols:
-            output.append(mat[i][j])
-            print(mat[i][j])
-            if down:
-                i += 1
-                j -= 1
-                
-                if i >= rows or j < 0:
-                    if i >= rows:
-                        i -= 1
-                        j += 2
-                    if j < 0:
-                        j += 1
-                    down = False
-            else:
-                i -= 1
+        up = True
+        for i in range(len(mat[0])):
+            intermediate_result = []
+            k = i
+            j = 0
+            while k >= 0 and j < len(mat):
+                intermediate_result.append(mat[j][k])
+                k -= 1
                 j += 1
-                if i < 0 or j >= cols:
-                    if i < 0 and j >= cols:
-                        i += 2
-                        j -= 1
-                    elif i < 0:
-                        i += 1
-                    elif j >= cols:
-                        j -= 1
-                    down = True
+            
+            if up:
+                intermediate_result.reverse()
+            
+            up = not up
+            
+            output += intermediate_result
         
-            print(i, j)
+        for i in range(1, len(mat)):
+            intermediate_result = []
+            j = i
+            k = len(mat[0]) - 1
+            
+            while j < len(mat) and k >= 0:
+                intermediate_result.append(mat[j][k])
+                j += 1
+                k -= 1
+            
+            if up:
+                intermediate_result.reverse()
+            up = not up
+            
+            output += intermediate_result
+                
+        
         return output
     '''
                         
