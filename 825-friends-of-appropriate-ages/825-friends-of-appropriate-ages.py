421d5dc49e34227ce7ee2adf3d66d3187bc94daf
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-12 10:35:59 +0200

    Time: 900 ms (26.98%), Space: 14.9 MB (53.72%) - LeetHub

--- 825-friends-of-appropriate-ages/825-friends-of-appropriate-ages.py
+++ 825-friends-of-appropriate-ages/825-friends-of-appropriate-ages.py
@@ -4,13 +4,14 @@ class Solution:
         total_friends = 0
         age_counter = Counter(ages)
         print(age_counter)
-        for age in ages:
+        for age in age_counter:
+            num_ages = age_counter[age]
             for friend_age in age_counter:
                 if not (0.5 * age + 7 >= friend_age) and not (friend_age > age) and not(friend_age > 100 and age < 100):    
                     if age != friend_age:
-                        total_friends += age_counter[friend_age]
+                        total_friends += num_ages*age_counter[friend_age]
                     else:
-                        total_friends += age_counter[friend_age] - 1
+                        total_friends += num_ages*(age_counter[friend_age] - 1)
         
         return total_friends
         
