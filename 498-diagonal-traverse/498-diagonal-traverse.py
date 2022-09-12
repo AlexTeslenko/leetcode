c051631568a96e7d13b3e3e4a3b92e7a7d0de8d1
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-12 13:34:41 +0200

    Time: 194 ms (96.17%), Space: 17.5 MB (68.14%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.py
+++ 498-diagonal-traverse/498-diagonal-traverse.py
@@ -1,41 +1,41 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         output = []
+        rows, cols = len(mat), len(mat[0])
         up = True
-        for i in range(len(mat[0])):
-            intermediate_result = []
-            k = i
-            j = 0
-            while k >= 0 and j < len(mat):
-                intermediate_result.append(mat[j][k])
-                k -= 1
-                j += 1
+        for col in range(cols):
+            j = col
+            row = 0
+            cur_diag = []
+            while j >= 0 and row < rows:
+                cur_diag.append(mat[row][j])
+                j -= 1
+                row += 1
             
             if up:
-                intermediate_result.reverse()
-            
+                cur_diag.reverse()
             up = not up
-            
-            output += intermediate_result
+            output += cur_diag
         
-        for i in range(1, len(mat)):
-            intermediate_result = []
-            j = i
-            k = len(mat[0]) - 1
+        for row in range(1, rows):
+            j = row
+            col = cols - 1
+            cur_diag = []
             
-            while j < len(mat) and k >= 0:
-                intermediate_result.append(mat[j][k])
+            while j < rows and col >= 0:
+                cur_diag.append(mat[j][col])
                 j += 1
-                k -= 1
+                col -= 1
             
             if up:
-                intermediate_result.reverse()
+                cur_diag.reverse()
             up = not up
-            
-            output += intermediate_result
+            output += cur_diag
                 
-        
         return output
+            
+            
+                
                 
         
         
