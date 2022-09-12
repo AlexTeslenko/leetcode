2156dfda748387cdd36c14533b2fd9b0c5c1595e
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-12 21:23:58 +0200

    Time: 3627 ms (5.17%), Space: 38.8 MB (30.54%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:   
+        output = []
+        
+        diags = {}
+        for row in range(len(nums)):
+            for col in range(len(nums[row])):
+                if row + col not in diags:
+                    diags[row + col] = []
+                diags[row + col].append(nums[row][col])
+        
+        for item in diags.values():
+            output += reversed(item)
+        
+                    
+        return output
+                
+        
