5fe1b6688c822d2da9335d08684276fe812ea7a0
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-14 12:50:39 +0200

    Time: 2355 ms (9.32%), Space: 33.7 MB (23.56%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -12,3 +12,17 @@ class Solution:
                     return True
         
         return False
+    
+    '''
+        mods = {0:-1}
+        cur_sum = 0
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            mod = cur_sum % k
+            if mod not in mods :
+                mods[mod] = i
+            elif i - mods[mod] >= 2:
+                return True
+        
+        return False
+    '''
