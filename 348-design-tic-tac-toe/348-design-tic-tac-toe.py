95b0ee0251791cd84787d619abb3875dd3a2b8d4
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-15 10:04:40 +0200

    Time: 851 ms (5.07%), Space: 16.9 MB (7.94%) - LeetHub

---
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -0,0 +1,53 @@
+class TicTacToe:
+
+    def __init__(self, n: int):
+        self.n = n
+        self.board = [[0 for i in range(n)] for _ in range(n)]
+        self.cols = {i:0 for i in range(n)}
+        self.rows = {i:0 for i in range(n)}
+        self.diags = {0: 0, 1: 0}
+        
+
+    def move(self, row: int, col: int, player: int) -> int:
+        if player == 1:
+            self.board[row][col] = 1
+        else:
+            self.board[row][col] = -1
+            
+        self.cols[col] += self.board[row][col]
+        self.rows[row] += self.board[row][col]
+            
+        if col + row == self.n - 1:
+            self.diags[1] += self.board[row][col]
+        if row == col:
+            self.diags[0] += self.board[row][col]
+        
+        #print(self.board)
+        #print(self.cols)
+        #print(self.rows)
+        for col in self.cols:
+            if self.cols[col] == self.n:
+                return 1
+            elif self.cols[col] == -self.n:
+                return 2
+        
+        for row in self.rows:
+            if self.rows[row] == self.n:
+                return 1
+            elif self.rows[row] == -self.n:
+                return 2
+        
+        for diag in self.diags:
+            if self.diags[diag] == self.n:
+                return 1
+            elif self.diags[diag] == -self.n:
+                return 2
+        return 0
+        
+            
+        
+
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
