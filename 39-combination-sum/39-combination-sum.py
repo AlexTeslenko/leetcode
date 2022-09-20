05744f9cb3bb07f8cae7a52fda3936e6ad09aee0
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-20 16:47:41 +0200

    Time: 185 ms (24.73%), Space: 14.1 MB (69.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,6 +2,35 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         output = []
         
+        def recursive_find(candidates, cur_target, cur_comb, start):
+            if cur_target < 0:
+                return
+            
+            if cur_target == 0:
+                output.append(cur_comb[:])
+                return
+                
+            for i in range(start, len(candidates)):
+                cur_comb.append(candidates[i])
+                
+                recursive_find(candidates, cur_target-candidates[i], cur_comb[:], i)
+                cur_comb.pop()
+            
+        
+        recursive_find(candidates, target, [], 0)
+        
+        return output
+                
+        
+        
+        
+        
+        
+        
+        
+        '''
+        output = []
+        
         def recursive_find(candidates, cur_idx, cur_target, cur_comb):
             if cur_target < 0 or cur_idx >= len(candidates):
                 return
@@ -20,4 +49,5 @@ class Solution:
         recursive_find(candidates, 0, target, [])
         
         return output
+        '''
         
