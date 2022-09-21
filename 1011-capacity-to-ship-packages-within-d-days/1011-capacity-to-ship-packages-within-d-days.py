3b515b9cfe7c98304464fa4081972cd17af8037b
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-21 17:47:43 +0200

    Time: 1388 ms (17.14%), Space: 17.2 MB (35.40%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,29 +1,32 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         
-        def check_feasible(capacity):
-            cur_cap = 0
-            cur_days = 0
+    
+        def check_feaibility(weights, capasity, days):
             
+            cur_weights = 0
             for w in weights:
-                cur_cap += w
-                if cur_cap > capacity:
-                    cur_cap = w
-                    cur_days += 1
-                
-                if cur_days >= days:
-                    return False
+                cur_weights += w
+                if cur_weights > capasity:
+                    days -= 1
+                    cur_weights = w
+            
+            if days <= 0:
+                return False
             
             return True
         
-        low, high = max(weights), sum(weights)
-        while low < high:
-            middle = low + (high - low) // 2
-            feasible = check_feasible(middle)
-            if feasible:
-                high = middle
+        min_cap, max_cap = max(weights), sum(weights)
+        
+        while min_cap < max_cap:
+            middle_cap = min_cap + (max_cap - min_cap) // 2
+            feacibility = check_feaibility(weights, middle_cap, days)
+            if feacibility:
+                max_cap = middle_cap
             else:
-                low = middle + 1
-            
-        return low
+                min_cap = middle_cap + 1
+        
+        return max_cap
+                
+                
             
