728ba8d84c8ed5e5b9a7906466fa2bc5039c7e82
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-09-23 21:27:30 +0200

    Time: 1263 ms (86.69%), Space: 25.3 MB (37.04%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -14,23 +14,12 @@ class Solution:
         
         '''
         
-        n = len(temperatures)
-        hottest = 0
-        answer = [0] * n
-        
-        for curr_day in range(n - 1, -1, -1):
-            current_temp = temperatures[curr_day]
-            if current_temp >= hottest:
-                hottest = current_temp
-                continue
-            
-            days = 1
-            while temperatures[curr_day + days] <= current_temp:
-                # Use information from answer to search for the next warmer day
-                days += answer[curr_day + days]
-            answer[curr_day] = days
-
-        return answer
-
+        ans, s = [0]*len(temperatures), deque()
+        for cur, cur_tmp in enumerate(temperatures):
+            while s and cur_tmp > temperatures[s[-1]]:
+                ans[s[-1]] = cur - s[-1]
+                s.pop()
+            s.append(cur)
+        return ans
                     
         
