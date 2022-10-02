090eb23183bf190f04e1722537a6a8eac1400c91
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-10-02 08:34:47 +0200

    Time: 69 ms (58.61%), Space: 14 MB (24.83%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return nums
+        
+        peack = -1
+        for i in range(len(nums)-1, 0, -1):
+            print(i)
+            if nums[i] > nums[i-1]:
+                
+                peack = i
+                break
+        
+        before_peack_val = nums[peack - 1]
+        i = peack
+        while i < len(nums) and nums[i] > before_peack_val:
+            i += 1
+        
+        nums[peack-1], nums[i-1] = nums[i-1], nums[peack-1]
+        print(peack)
+        if peack == -1:
+            peack = 0
+            
+        nums[peack:] = reversed(nums[peack:])
+        
+        
+            
