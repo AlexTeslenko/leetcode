cf800fcea1176dfd9a54518a7b9074b19b0abfc3
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-10-03 15:08:34 +0200

    Time: 839 ms (24.18%), Space: 21.8 MB (97.44%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.py
@@ -4,7 +4,6 @@ class Solution:
             if nums[abs(nums[i])-1] > 0:
                 nums[abs(nums[i])-1] = nums[abs(nums[i])-1] * (-1)
         
-        print(nums)
         output = []
         for i in range(len(nums)):
             if nums[i] > 0:
