23e6f3b49e9e9884afe6f6de92824835f4fcf51b
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-10-09 11:38:05 +0200

    Time: 145 ms (17.25%), Space: 14 MB (19.38%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,11 +1,9 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        not_found_substr = False
         
         def form_str(source, cur_target):
             if not cur_target:
                 return 0
-            
             
             i, j = 0, 0          
             found_substr = False
@@ -16,7 +14,6 @@ class Solution:
                 
                 i += 1
                
-                
             if found_substr:
                 num_sub = form_str(source, cur_target[j:])
                 if num_sub == -1:
@@ -24,7 +21,6 @@ class Solution:
                 else:
                     return 1 + num_sub  
             else:
-                not_found_substr = True
                 return -1
         
         return form_str(source, target)
