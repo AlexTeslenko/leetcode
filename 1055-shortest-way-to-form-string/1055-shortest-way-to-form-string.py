4f42df4432c7b225d0c4acb09a30b395b74c08de
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-10-09 11:36:53 +0200

    Time: 160 ms (12.54%), Space: 13.9 MB (40.60%) - LeetHub

---
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -0,0 +1,40 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        not_found_substr = False
+        
+        def form_str(source, cur_target):
+            if not cur_target:
+                return 0
+            
+            
+            i, j = 0, 0          
+            found_substr = False
+            while i < len(source) and j < len(cur_target):
+                if source[i] == cur_target[j]:
+                    found_substr = True
+                    j += 1
+                
+                i += 1
+               
+                
+            if found_substr:
+                
+                print(cur_target[j:])
+                num_sub = form_str(source, cur_target[j:])
+                if num_sub == -1:
+                    return -1
+                else:
+                    return 1 + num_sub  
+            else:
+                print("not found")
+                not_found_substr = True
+                return -1
+        
+        print(not_found_substr)
+        return form_str(source, target)
+        
+        if not_found_substr:
+            return -1
+                    
+                    
+            
