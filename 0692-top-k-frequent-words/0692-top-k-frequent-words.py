3a28a37aefa59e5853e6b90ceefc1e0102358ef4
AlexTeslenko <59090135+AlexTeslenko@users.noreply.github.com>
2022-10-13 10:21:04 +0200

    Time: 130 ms (22.49%), Space: 14.1 MB (27.29%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        words_num = Counter(words)
+        nums_word = {}
+        for word, num in words_num.items():
+            if num not in nums_word:
+                nums_word[num] = []
+            nums_word[num].append(word)
+        
+        for num in nums_word:
+            nums_word[num].sort()
+        
+        print(nums_word)
+        
+        nums_sorted = list(nums_word.items())
+        nums_sorted.sort(key=lambda x: x[0], reverse=True)
+        
+        output = []
+        i = 0
+        while i < k:
+            for item in nums_sorted:
+                for word in item[1]:
+                    i += 1
+                    output.append(word)
+                    if i == k:
+                        return output
+            
+        
+        return output
+        
